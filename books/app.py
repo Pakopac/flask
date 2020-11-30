5fcd03056427aa13a4d5a1734a04bb494925c0ae
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-11-30 11:30:01 +0100

    Set json in a file

--- books/app.py
+++ books/app.py
@@ -1,34 +1,30 @@
 from flask import Flask, render_template
 from jinja2 import Template 
+import json
 app = Flask(__name__)
 
-book=[
-	{
-		'id':1,
-		'titre' : 'un titre',
-	},
-	{
-		'id':2,
-		'titre': 'un autre titre random',
-	}
-]
+# json importation
+books_file = "assets/books.json"
+with open(books_file) as json_file:
+    books_load = json.load(json_file)
+books_dump = json.dumps(books_load)
+books_json = json.loads(books_dump)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/api/books', methods=["GET"])
 def books():
-    return render_template('books.html', book=book)
+    return render_template('books.html', books=books_json)
 
 @app.route('/api/book/id/<id>')
 def book_by_id(id):
-    return render_template('book_by_id.html', books=book, id=id)
+    return render_template('book_by_id.html', books=books_json, id=id)
 
 @app.route('/api/book/title/<title>')
 def book_by_title(title):
-    return render_template('book_by_title.html', books=book, title=title)
+    return render_template('book_by_title.html', books=books_json, title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
